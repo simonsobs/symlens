e059bc94bd01876aec6f8f04d83be387613ee664
Mathew Madhavacheril <mathewsyriac@gmail.com>
2020-07-24 15:45:38 -0400

    Bump version: 0.2.4 → 0.3.0

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/simonsobs/symlens',
-    version='0.2.4',
+    version='0.3.0',
     zip_safe=False,
 )
